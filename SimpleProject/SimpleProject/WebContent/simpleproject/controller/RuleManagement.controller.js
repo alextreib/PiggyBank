5b7b9cadabcfefa253fab21976eab45185e502ec
Andreas Hefner <andreas.hefner@hotmail.de>
2016-11-26 21:49:57 +0000

    RuleManagment - base implementation

--- SimpleProject/SimpleProject/WebContent/simpleproject/controller/RuleManagement.controller.js
+++ SimpleProject/SimpleProject/WebContent/simpleproject/controller/RuleManagement.controller.js
@@ -1,11 +1,56 @@
-sap.ui.controller("simpleproject.controller.RuleManagement", {
+var _caller;
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf simpleproject.App
-*/
-	onInit: function() {
+sap.ui.define([
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/Controller",
+    "simpleproject/model/models"
+  ], function (JSONModel, Controller, models) {
+    "use strict";
 
-	}
+   return Controller.extend("simpleproject.controller.RuleManagement", {
+        onInit: function () {
+        var router =  this.getOwnerComponent().getRouter();
+		router.getRoute("RuleManagement").attachMatched(this._onRouteMatched,this);
+        },
+        _onRouteMatched : function(oEvent){
+		var oArgs, oView;
+		oArgs = oEvent.getParameter("arguments");
+		oView = this.getView();
+		_caller = this.getView();
+    	var me = this;
+        models.getList("rules", function(ruleList) {
+    		console.log(ruleList);
+            me.getView().setModel(ruleList);
+        });
+        },
+        onNavBack: function(oEvent){
+        	sap.ui.controller("simpleproject.controller.Account").onNavBack();
+        },
+        onNewRuleCreate:function (oEvent){
+        	
+        	
+        //	var oDialog = sap.ui.xmlfragment(this.getView().getId(), "simpleproject.fragment.NewRuleDialog", this);
+				// connect dialog to view (models, lifecycle)
+			//	this.getView().addDependent(oDialog);
+			
+				if(!this._dialog){
+			this._dialog =  sap.ui.xmlfragment( "simpleproject.fragment.NewRuleDialog", this);
+		//	this.getView().addDependent(this._dialog);
+		}
+		// open dialog
+		jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._dialog);
+		this._dialog.open();
+
+ 
+        },
+        saveRule :function(oEvent)
+        {	
+        	var ruleName =  sap.ui.getCore().byId("tf_ruleName").getValue();
+        	var border = sap.ui.getCore().byId("tf_border").getValue();
+        	var purpose = sap.ui.getCore().byId("tf_purpose").getValue();
+        	alert("saved!");	
+        	this._dialog.close();
+        }
+        
+    });
 });
