32219f3e5dc33ee8d211097d3426f07b8c98d0fa
Andreas Hefner <andreas.hefner@hotmail.de>
2016-11-26 22:19:48 +0000

    Wishlist - Added

--- SimpleProject/SimpleProject/WebContent/simpleproject/controller/Wishlist.controller.js
+++ SimpleProject/SimpleProject/WebContent/simpleproject/controller/Wishlist.controller.js
@@ -1,11 +1,54 @@
-sap.ui.controller("simpleproject.controller.Wishlist", {
+var _caller;
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf simpleproject.App
-*/
-	onInit: function() {
+sap.ui.define([
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/Controller",
+    "simpleproject/model/models"
+  ], function (JSONModel, Controller, models) {
+    "use strict";
 
-	}
+   return Controller.extend("simpleproject.controller.Wishlist", {
+        onInit: function () {
+        var router =  this.getOwnerComponent().getRouter();
+		router.getRoute("WishList").attachMatched(this._onRouteMatched,this);
+        },
+        _onRouteMatched : function(oEvent){
+		var oArgs, oView;
+		oArgs = oEvent.getParameter("arguments");
+		oView = this.getView();
+		_caller = this.getView();
+    	var me = this;
+        models.getList("Wishes", function(wishlist) {
+    		console.log(wishlist);
+            me.getView().setModel(new JSONModel(wishlist));
+        });
+        },
+        onNavBack: function(oEvent){
+        	sap.ui.controller("simpleproject.controller.Account").onNavBack();
+        },
+        onNewWishCreate:function (oEvent){
+        //	var oDialog = sap.ui.xmlfragment(this.getView().getId(), "simpleproject.fragment.NewRuleDialog", this);
+				// connect dialog to view (models, lifecycle)
+			//	this.getView().addDependent(oDialog);
+			
+				if(!this._dialog){
+			this._dialog =  sap.ui.xmlfragment( "simpleproject.fragment.CreateNewWish", this);
+		//	this.getView().addDependent(this._dialog);
+		}
+		// open dialog
+		jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._dialog);
+		this._dialog.open();
+
+ 
+        },
+        saveWish :function(oEvent)
+        {	
+        	var product =  sap.ui.getCore().byId("tf_product").getValue();
+        	var price = sap.ui.getCore().byId("tf_price").getValue();
+        	var date = sap.ui.getCore().byId("tf_date").getValue();
+        	alert("saved!");	
+        	this._dialog.close();
+        }
+        
+    });
 });
